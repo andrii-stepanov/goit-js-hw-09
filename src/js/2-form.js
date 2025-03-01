534e128a0e16ee0427d5255d96a3cb0e0f7c2bb7
andrii-stepanov <andriilstdev@gmail.com>
2025-03-01 22:59:23 +0200

    hw9

---
+++ src/js/2-form.js
@@ -0,0 +1,46 @@
+const form = document.querySelector('.feedback-form');
+const STORAGE_KEY = 'feedback-form-state';
+
+const formData = {
+    email: "",
+    message: "",
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    const savedData = localStorage.getItem(STORAGE_KEY);
+    if (savedData) {
+        try {
+            const parsedData = JSON.parse(savedData);
+            formData.email = parsedData.email || '';
+            formData.message = parsedData.message || '';
+
+            form.elements.email.value = formData.email || '';
+            form.elements.message.value = formData.message || '';
+        } catch (error) {
+            console.log('Error parsing localStorage data:', error);
+        }
+    }
+});
+
+form.addEventListener('input', event => {
+    formData[event.target.name] = event.target.value.trim();
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+});
+
+form.addEventListener('submit', event => {
+    event.preventDefault();
+
+    const { email, message } = formData;
+
+    if (!email || !message) {
+        alert('Fill please all fields');
+        return;
+    }
+
+    console.log('Submitted data:', formData);
+
+    localStorage.removeItem(STORAGE_KEY);
+    form.reset();
+    formData.email = '';
+    formData.message = '';
+})
